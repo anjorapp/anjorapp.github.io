b5b399c24cf06a0569534d06e0d6c57c154a28d7
anjorapp <anjomingo@gmail.com>
2020-05-19 01:56:18 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
 
   $('a[href^="#"]').click(function () {
     $root.animate({
-        scrollTop: $($.attr(this, "href")).offset().top
+        scrollTop: $($.attr(this, "href")).offset().top - 82
       },
       300
     );
