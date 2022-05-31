fe7a7334d7d5005993af3f213ea3ab0123b1789c
anjorapp <anjomingo@gmail.com>
2022-05-31 20:02:39 +0800

    Upload

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+
+  new WOW().init();
+
+
+  $(this).scrollTop(0);
+
+  sticky();
+
+  $(window).scroll(function () {
+    sticky();
+  });
+
+  function sticky() {
+    if ($(document).scrollTop() > 90) {
+      $(".main-menu").addClass("sticky");
+    } else {
+      $(".main-menu").removeClass("sticky");
+    }
+  }
+
+  var $root = $("html, body");
+
+  $('a[href^="#"]').click(function () {
+    $root.animate({
+        scrollTop: $($.attr(this, "href")).offset().top - 82
+      },
+      300
+    );
+    return false;
+  });
+});
+
+$(window).on("load", function () {
+  $('.preloader').addClass('hidden')
+  $('body').addClass('loaded')
+});
