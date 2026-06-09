67f5009e6705b31149209ce5421df2c5a8abd9e3
anjorapp <anjomingo@gmail.com>
2026-06-09 19:37:15 +0800

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,18 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <div className="hero-banner relative w-full h-screen flex items-center justify-center">
+      <div className="hero-banner relative w-full h-svh flex items-center justify-center">
         <div id="pt" className="canvas -z-0"></div>
         <div className="z-10 flex flex-col items-center justify-center">
           <span className="font-mono text-sm text-primary">// portfolio v2.0</span>
-          <h1 className="text-9xl font-bold font-display text-primary-bright">
+          <h1 className="text-7xl md:text-9xl font-bold font-display text-primary-bright">
             Anjorapp
           </h1>
-          <p className="text-center text-2xl font-bold font-display tracking-widest text-muted">
+          <p className="text-center text-2xl md:text-2xl font-bold font-display tracking-widest text-muted">
             Anjo Ralph Mingo <br />
             Full-Stack Web Developer
           </p>
-          <div className="flex gap-8 mt-8 font-display tracking-widest">
+          <div className="flex gap-6 mt-8 font-display tracking-widest">
             <a href="#work"
               className="border border-primary/80 bg-primary/80 text-foreground hover:bg-primary px-6 py-3 transition-colors duration-200 leading-none">
               View my work
@@ -31,9 +31,15 @@ export default function Home() {
           <div className="scroll-line"></div>
         </div>
       </div >
-
       <Script src="/lib/pt.min.js" strategy="beforeInteractive" />
       <Script src="/lib/canvas.js" strategy="afterInteractive" />
+
+
+      <section className="">
+        <div>
+
+        </div>
+      </section>
     </>
   );
 }
