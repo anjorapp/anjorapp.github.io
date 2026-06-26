db8397bde5fd420d018e0abcbc073049b0f05969
anjorapp <anjomingo@gmail.com>
2026-06-27 02:51:43 +0800

    cleaner fade in

--- app/page.tsx
+++ app/page.tsx
@@ -333,7 +333,7 @@ export default function Home() {
 
       <div className="section-divider"></div>
 
-      <section className="py-16 bg-secondary scroll-mt-[56px]">
+      <section className="py-16 bg-secondary scroll-mt-[56px] overflow-hidden">
         <div className="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 !w-fit justify-center items-start gap-10">
           {skills.map((skill, index) => (
             <div key={index} data-aos="fade-up" data-aos-delay={index * 100} className="w-fit max-w-[238px] flex flex-col gap-3 justify-center text-center">
